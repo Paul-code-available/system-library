8d179b55db8a8e78e750214643746bc01ceecb6a
Paul-code-available <paulsanchezsolisvega10@gmail.com>
2026-02-11 11:29:33 -0800

    Avance del login

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -1,10 +1,14 @@
 package views;
 
+import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 
@@ -26,16 +30,49 @@ public class LoginView extends JFrame {
 	
 	private void inicializarComponentes() {
 		crearBotones();
+		crearFormulario();
 	}
 	
 	private void crearBotones() {
 		
 		JButton boton = new JButton("Iniciar sesión");
-		boton.setBounds(140, 200, 120, 25);
+		boton.setBounds(250, 10, 117, 25);
 		boton.setFont(fuente);
 		
 		add(boton);
 	
+	}
+	
+	private void crearFormulario() {
+		JLabel lblSaludo = new JLabel("Bienvenido!");
+		lblSaludo.setFont(fuente);
+		lblSaludo.setBounds(10, 0, 200, 40);
+		add(lblSaludo);
+		
+		int lblX = 10, y = 170, txtX = 100;
+		
+		JLabel lblEmail = new JLabel("Email: ");
+		lblEmail.setFont(fuente);
+		lblEmail.setBounds(lblX, y, 200, 40);
+		add(lblEmail);
+		
+		JTextField txtEmail = new JTextField();
+		txtEmail.setFont(fuente);
+		txtEmail.setBounds(txtX, y, 200, 40);
+		add(txtEmail);
+
+		y += 70;
+		
+		JLabel lblContrasena = new JLabel("Contraseña: ");
+		lblContrasena.setFont(fuente);
+		lblContrasena.setBounds(lblX, y, 200, 40);
+		add(lblContrasena);
+		
+		JPasswordField contrasena = new JPasswordField();
+		contrasena.setFont(fuente);
+		contrasena.setBounds(txtX, y, 200, 40);
+		add(contrasena);
+		
 	}
 	
 	
